9f4e05953fc576d830d7a45dd30e5f23e300d1aa
Treeshanie Goonewardena <treeshanie.20234027@iit.ac.lk>
2025-03-08 17:10:17 +0530

    Page editor sample update

---
+++ PageEditor/PageEditor_ST1.js
@@ -0,0 +1,12 @@
+
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => document.getElementById(id).innerHTML = data)
+        .catch(error => console.error(`Error loading ${file}:`, error));
+        }
+
+
+// Load Navbar and Footer
+loadComponent("navbar-container", "../NavBar/navbar.html");
+loadComponent("footer-container", "../Footer/footer.html");
